8b3c8e9852d6bc3080f3496b004bec41827d6077
Devarajaz <devarajmuddha@gmail.com>
2025-10-17 16:35:16 +0530

    Added some more API's

--- src/app.js
+++ src/app.js
@@ -18,6 +18,34 @@ try {
  }
 });
 
+app.get("/user", async (req, res) => {
+    const userEmail = req.body.emailId;
+
+    try {
+        const users = await User.find({ emailId: userEmail });
+        if (users.length === 0) {
+            res.status(404).send("User not found")
+        }else{
+            res.send(users);
+        }
+    } catch (error) {
+        res.status(400).send("something went wrong")
+    }
+});
+
+
+app.get("/feed", async (req, res) => {
+
+    try {
+        const users = await User.find({});
+        res.send(users);
+    } catch (error) {
+        res.status(400).send("Something went wrong");
+    }
+
+});
+
+
 connectDB()
    .then(() => {
     console.log("database connected");
